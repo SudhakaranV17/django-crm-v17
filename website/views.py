590c51f81b8f8e813dbc4b990fd59bf1eb6ef541
SudhakaranV17 <sudhakaranv17@gmail.com>
2023-06-04 17:06:05 +0530

    added register feature and created record model

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages 
+from .forms import SignUpForm
+from .models import Record
+
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST["username"]
         password = request.POST["password"]
@@ -16,4 +20,26 @@ def home(request):
             messages.error(request,"There was an error in login. Please try again....")
             return redirect("home")
     else:
-        return render(request,"home.html",{})
+        return render(request,"home.html",{'records':records})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You are logged out.")
+    return redirect("home")
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You are successfully Registered!")
+            return redirect("home")
+    else:
+        form = SignUpForm()            
+        return render(request,"register.html",{'form':form}) 
+    return render(request,"register.html",{'form':form}) 
